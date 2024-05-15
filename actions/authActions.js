dc15da4ccd4003d67e9e02460ac44f246ca59217
J-Skarbek <john@snarkmediaagency.com>
2024-05-15 15:02:04 -0500

    Add basic password hashing

--- actions/authActions.js
+++ actions/authActions.js
@@ -1,5 +1,8 @@
 'use server';
 
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+
 export async function signup(prevState, formData) {
   const email = formData.get('email');
   const password = formData.get('password');
@@ -23,4 +26,6 @@ export async function signup(prevState, formData) {
     }
   }
 
+  const hashedPassword = hashUserPassword(password);
+  createUser(email, hashedPassword);
 }
